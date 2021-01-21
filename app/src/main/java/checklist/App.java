e81623651a483a893c6801d9ede40115abaa39f5
anton0xf <anton0xf@gmail.com>
2021-01-22 00:11:42 +0500

    create: print created file name

--- app/src/main/java/checklist/App.java
+++ app/src/main/java/checklist/App.java
@@ -1,7 +1,43 @@
 package checklist;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class App {
+
+    public static final String CREATE_COMMAND = "create";
+
     public static void main(String[] args) {
-        System.err.println("Checklist created");
+        if(args.length == 0) {
+            printHelp();
+            System.exit(1);
+        }
+        String command = args[0];
+        List<String> commandArgs = Arrays.stream(args)
+                .skip(1)
+                .collect(Collectors.toList());
+        switch (command) {
+            case CREATE_COMMAND -> create(commandArgs);
+            default -> {
+                printHelp();
+                System.exit(1);
+            }
+        }
+    }
+
+    private static void printHelp() {
+        // TODO print help message and usage to stderr
+    }
+
+    private static void create(List<String> args) {
+        if(args.isEmpty()) {
+            // TODO print command usage
+            return;
+        }
+        String name = args.get(0);
+        File file = new File(name + ".checklist");
+        System.err.println(String.format("Checklist '%s' created", file.getPath()));
     }
 }
--- app/src/test/java/checklist/AppTest.java
+++ app/src/test/java/checklist/AppTest.java
@@ -8,10 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AppTest {
     @Test
     void create() throws Exception {
-        String[] args = {"create", "new-checklist"};
+        String[] args = {"create", "buy"};
         String out = tapSystemErrNormalized(() -> App.main(args));
-        // TODO print created file name
-        assertEquals("Checklist created\n", out);
+        assertEquals("Checklist 'buy.checklist' created\n", out);
         // TODO check that file created
         // TODO check file content
     }
